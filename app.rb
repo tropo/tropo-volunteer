74f31ba4373116fde7aa7a9c030e272b9e36969c
Mark S <mark@mark-s.net>
2010-09-10 21:54:05 -0400

    push

--- app.rb
+++ app.rb
@@ -9,9 +9,7 @@ post '/index.json' do
     t.on :event => 'error', :next => '/error.json'     # For fatal programming errors. Log some details so we can fix it
     t.on :event => 'hangup', :next => '/hangup.json'   # When a user hangs or call is done. We will want to log some details.
     t.on :event => 'continue', :next => '/process_zip.json'
-    t.say "Well hello there."
-
-    t.ask :name => 'zip', :bargein => true, :timeout => 7, :required => true, :attempts => 4,
+    t.ask :name => 'zip', :bargein => true, :timeout => 60, :required => true, :attempts => 4,
         :say => [{:event => "timeout", :value => "Sorry, I did not hear anything."},
                  {:event => "nomatch:1 nomatch:2 nomatch:3", :value => "That wasn't a five-digit zip code."},
                  {:value => "In what zip code would you like to search for volunteer opportunities in?."}],
@@ -24,10 +22,10 @@ post '/process_zip.json' do
   t = Tropo::Generator.new(:voice => "kate")
     t.on  :event => 'error', :next => '/error.json'
     t.on  :event => 'hangup', :next => '/hangup.json'
-    t.on  :event => 'continue', :next => '/process_input.json'
+    t.on  :event => 'continue', :next => '/process_selection.json'
     t.say v[:result][:actions][:zip][:value]
     params = {
-      :num => "10",
+      :num => "9",
       :output => "json",
       :vol_loc => v[:result][:actions][:zip][:value].gsub(" ",""),
       :vol_startdate => Time.now.strftime("%Y-%m-%d"),
@@ -36,37 +34,42 @@ post '/process_zip.json' do
       
     url = "http://www.allforgood.org/api/volopps?key=tropo"
     params.each{|key,value| url << "&#{key}=#{value}"}
-    t.say(url)
+    
     begin
-      data = JSON.parse(open(url).read)
-      puts data
-      t.say "#{data["items"].size} opportunities found. I'll read them to you"
+      session[:data] = JSON.parse(open(url).read)
     rescue => e
       puts e # print error to sinatra console
-      t.say "It looks like something went wrong with our data source. Please try again later. "
+      t.say "It looks like something went wrong with our data source. Please try again later. Goodbye."
       t.hangup
     end
+    
+    if session[:data]["items"].size > 0
+      t.say "Here are #{session[:date]["items"].size} opportunities. Press the opportunity number you want more information about."
+      session[:data]["items"].each_with_index do |item,i|
+        t.ask :name => 'selection', :bargein => true, :timeout => 60, :required => false, :attempts => 2,
+            :say => [{:event => "nomatch:1 nomatch:2 nomatch:3", :value => "That wasn't a one-digit opportunity number."},
+                     {:value => "Opportunity #{i}: #{item["title"]}"}],
+                      :choices => { :value => "[1 DIGITS]"}
+      end
+    else
+      t.say "No volunteer opportunities found in zip code. Please try calling back later. Goodbye."
+    end
+        
     t.hangup
   t.response  
 end
 
-# post '/say_page_of_tweets.json' do
-#   t = Tropo::Generator.new
-#     t.on  :event => 'error', :next => '/error.json'  
-#     t.on  :event => 'hangup', :next => '/hangup.json'
-#     t.on  :event => 'continue', :next => '/say_page_of_tweets.json'
-#     t.say "I'm about to read you the"
-#     t.say say_as_ordinal(session[:page])
-#     t.say " #{session[:count]} tweets by #{session[:user]}."
-#     source = "http://twitter.com/statuses/user_timeline/#{session[:user]}.rss?count=#{session[:count]}&page=#{session[:page]}"
-#     rss = REXML::Document.new(open(source).read).root
-#     rss.root.elements.each("channel/item") { |element|
-#       t.say "Tweet from about #{Time.parse(element.get_text('pubDate').to_s).to_pretty}"
-#       t.say reformat_uris(element.get_text('title').to_s) + "," # comma for extra pause between tweets.
-#     }
-#     session[:page] += 1
-#   t.response
-# end
+post '/process_selection.json' do
+  v = Tropo::Generator.parse request.env["rack.input"].read
+  t = Tropo::Generator.new
+    t.on  :event => 'error', :next => '/error.json'  
+    t.on  :event => 'hangup', :next => '/hangup.json'
+    if v[:result][:actions][:selection][:value]
+      t.say "Opportunity #{v[:result][:actions][:zip][:value].to_i+1} information ..."
+    end
+    t.hangup
+  t.response
+end
 
 post '/hangup.json' do
   v = Tropo::Generator.parse request.env["rack.input"].read
