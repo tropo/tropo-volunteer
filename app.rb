24dc9de288b81e852ee44563d011f935aa5fda8e
Mark S <mark@mark-s.net>
2010-09-10 23:00:06 -0400

    push

--- app.rb
+++ app.rb
@@ -65,10 +65,9 @@ post '/process_selection.json' do
   t = Tropo::Generator.new
     t.on  :event => 'error', :next => '/error.json'  
     t.on  :event => 'hangup', :next => '/hangup.json'
-    if selection = v[:result][:actions][:selection][:value]
-      item = session[:data][selection.to_i-1]
-      
-      t.say "Information about opportunity ##{selection} (#{item["title"]}) is as follows: "
+    if v[:result][:actions][:selection][:value]
+      item = session[:data][v[:result][:actions][:selection][:value].to_i-1]
+      t.say "Information about opportunity #{item["title"]} is as follows: "
       t.say "From #{Time.parse(item["startDate"]).strftime("%a %m/%d at %I:%M %p")} to #{Time.parse(item["endDate"]).strftime("%a %m/%d at %I:%M %p")}" unless item["startDate"].empty? or item["endDate"].empty
       t.say "Contact "
       tinyurl = shorten_url(URI.unescape(item["xml_url"]))
